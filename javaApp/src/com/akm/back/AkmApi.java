4676286e4af510bd41a1de03ce41f6d452f90679
Math <philipponmathis@gmail.com>
2022-04-22 12:21:06 +0200

    Initial java App commit

---
+++ javaApp/src/com/akm/back/AkmApi.java
@@ -0,0 +1,48 @@
+package com.akm.back;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AkmApi {
+    private final String host = "localhost/";
+
+    /*
+    public static String request(Actions action){
+
+    }
+    */
+
+    private enum Methods {
+        POST, GET;
+    }
+
+    private enum Controllers {
+        LOGIN("login")
+        ;
+
+        public final String url;
+
+        Controllers(String url) {
+            this.url = url;
+        }
+    }
+
+    public enum Actions {
+        SIGNUP(Methods.POST, "signup", Controllers.LOGIN),
+        REGISTER_COMPANY(Methods.POST, "registercompany", Controllers.LOGIN),
+        GENERATE_CODE(Methods.POST, "generatecode", Controllers.LOGIN),
+        SIGN_IN(Methods.POST, "signin", Controllers.LOGIN),
+        GET_USER_DATA(Methods.POST, "getdata", Controllers.LOGIN)
+        ;
+
+        public final Methods method;
+        public final String url;
+        public final Controllers controller;
+
+        Actions(Methods method, String url, Controllers controller) {
+            this.method = method;
+            this.url = url;
+            this.controller = controller;
+        }
+    }
+}
