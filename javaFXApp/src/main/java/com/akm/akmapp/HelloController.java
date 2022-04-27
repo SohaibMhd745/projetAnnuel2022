df4de8c59c3a097f97eb7918f404f7388e6df5ba
SohaibMhd745 <sohaib.mohammad745@gmail.com>
2022-04-27 11:43:58 +0200

    Initial JavaFX app created

---
+++ javaFXApp/src/main/java/com/akm/akmapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.akm.akmapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
